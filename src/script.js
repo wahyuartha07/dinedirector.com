0ed87373fdcc5e2daa8b403dfb23eb6b1b70e3b2
Gib-byte <gibranabyan082@gmail.com>
2023-12-16 21:47:24 +0700

    Update HTML

--- src/script.js
+++ src/script.js
@@ -1,47 +1,62 @@
 let inventory = [];
 
-const form = document.getElementById('product-form');
-const inventoryList = document.getElementById('inventory-list');
+const form = document.getElementById("product-form");
+const inventoryList = document.getElementById("inventory-list");
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    addProduct();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  addProduct();
 });
 
 function addProduct() {
-    const productName = document.getElementById('product-name').value;
-    const productQuantity = parseInt(document.getElementById('product-quantity').value);
-    const productPrice = document.getElementById('product-price').value;
+  const productName = document.getElementById("product-name").value;
+  const productQuantity = parseInt(
+    document.getElementById("product-quantity").value
+  );
+  const productPrice = document.getElementById("product-price").value;
 
-    if (!productName || !productQuantity || !productLocation) {
-        alert('Please fill in all fields');
-        return;
-    }
+  if (!productName || !productQuantity || !productPrice) {
+    alert("Please fill in all fields");
+    return;
+  }
 
-    const product = {
-        name: productName,
-        quantity: productQuantity,
-        location: productLocation
-    };
+  const product = {
+    name: productName,
+    quantity: productQuantity,
+    price: productPrice,
+  };
 
-    inventory.push(product);
-    displayInventory();
-    form.reset();
+  inventory.push(product);
+  displayInventory();
+  form.reset();
 }
 
 function displayInventory() {
-    inventoryList.innerHTML = '';
+    const inventoryList = document.getElementById("inventory-list");
+    inventoryList.innerHTML = "";
+
+    const titleRow = inventoryList.insertRow();
+    titleRow.id = "tableHead"; 
+    const titleCells = ["No", "Nama", "Quantity", "Total Harga"];
+
+    titleCells.forEach((title) => {
+        const th = document.createElement("th");
+        th.textContent = title;
+        titleRow.appendChild(th);
+    });
 
     for (let i = 0; i < inventory.length; i++) {
         const product = inventory[i];
-        const productElement = document.createElement('div');
-        productElement.textContent = `Name: ${product.name}, Quantity: ${product.quantity}, Location: ${product.location}`;
-        inventoryList.appendChild(productElement);
+        const newRow = inventoryList.insertRow();
+
+        const cellNo = newRow.insertCell();
+        cellNo.textContent = i + 1;
+        const cellName = newRow.insertCell();
+        cellName.textContent = product.name;
+        const cellQuantity = newRow.insertCell();
+        cellQuantity.textContent = product.quantity;
+        const cellPrice = newRow.insertCell();
+        cellPrice.textContent = `Rp ${product.price},-`;
     }
 }
 
-function updateTotal() {
-    var quantity = document.getElementById("quantity").value;
-    var total = quantity * 10;
-    document.getElementById("total").innerHTML = "Rp " + total.toLocaleString() + ",-";
-}
